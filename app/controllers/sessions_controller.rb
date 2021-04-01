7bf7015e56947c116ab739e886c21f5a2a6844bd
Leo <efrenn98@gmail.com>
2021-03-31 18:46:38 -0700

    Added routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,13 @@
 class SessionsController < ApplicationController
+
+    def new 
+        @comedian= Comedian.new
+
+    end
+    
+    def destroy
+        session.clear
+        redirect_to "/login"
+      end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,12 @@ module ApplicationHelper
         !!session[:user_id]
     end
 
-    
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+
+    def redirect_if_logged_in
+        redirect_to products_path if logged_in?
+    end
+
 end
